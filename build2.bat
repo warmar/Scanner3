a73c57234e544a8b7403ac1cf76b362c29ecef16
Peter Marangos <lifelessmango@gmail.com>
2017-03-16 18:41:20 -0400

    Add build2.bat - new build method using pyinstaller

---
+++ build2.bat
@@ -0,0 +1,3 @@
+cd src
+C:\Users\Peter\AppData\Local\Programs\Python\Python35\Scripts\pyinstaller __main__.py --distpath "../distwin" --hidden-import queue -n "Scanner3" -i "Resources/Icon.ico" -w -F
+pause
