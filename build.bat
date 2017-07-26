1e46927abe2c42d2a0ce80972957a4f815433b18
Peter Marangos <lifelessmango@gmail.com>
2017-07-25 22:11:49 -0400

    Replace build.bat with build2.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 cd src
-py -3.4 setup.py py2exe
+C:\Users\Peter\AppData\Local\Programs\Python\Python35\Scripts\pyinstaller __main__.py --distpath "../distwin" --workpath "../distwin/temp" --specpath "../distwin/temp/" --hidden-import queue -n "Scanner3" -i "Resources/Icon.ico" -w -y -F
+rmdir "../distwin/temp" /s /q
 pause
