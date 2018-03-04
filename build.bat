13fdc11aa8de3a3a8c71aa9106d57185ff650e8c
Peter Marangos <lifelessmango@gmail.com>
2018-03-04 13:24:58 -0500

    build.bat: Run pyinstaller command directly

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 cd src
-C:\Users\Peter\AppData\Local\Programs\Python\Python35\Scripts\pyinstaller __main__.py --distpath "../distwin" --workpath "../distwin/temp" --specpath "../distwin/temp/" --hidden-import queue -n "Scanner3" -i "Resources/Icon.ico" -w -y -F
+pyinstaller __main__.py --distpath "../distwin" --workpath "../distwin/temp" --specpath "../distwin/temp/" --hidden-import queue -n "Scanner3" -i "Resources/Icon.ico" -w -y -F
 rmdir "../distwin/temp" /s /q
 pause
