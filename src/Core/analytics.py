b7b094daeeea778bb86915155b28597f298523d9
Peter Marangos <lifelessmango@gmail.com>
2018-07-25 01:59:45 -0400

    Core: More robust API validation + Add anonymous analytics

---
+++ src/Core/analytics.py
@@ -0,0 +1,36 @@
+import requests
+import secrets
+import time
+import os
+
+characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYXZ0123456789'
+
+PATH = 'Resources/clientid'
+
+
+def get_client_id():
+    # Reads or creates random client id (anonymous)
+    if os.path.exists(PATH):
+        with open(PATH, 'r') as id_file:
+            client_id = id_file.read()
+        return client_id
+
+    client_id = ''
+    for _ in range(24):
+        client_id += secrets.choice(characters)
+
+    with open(PATH, 'w+') as id_file:
+        id_file.write(client_id)
+
+    return client_id
+
+def report_launch():
+    client_id = get_client_id()
+    curr_time = round(time.time(), 2)
+    requests.get('https://scanner3.net/analytics?id=%s&launchTime=%s' % (client_id, curr_time))
+
+
+def report_close():
+    client_id = get_client_id()
+    curr_time = round(time.time(), 2)
+    requests.get('https://scanner3.net/analytics?id=%s&closeTime=%s' % (client_id, curr_time))
