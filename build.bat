3b78608ee5b864bdb01f0333f37b274c99cbab66
Warwick Marangos <warwick.marangos@gmail.com>
2018-09-17 22:45:41 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 cd src
 pyinstaller __main__.py --distpath "../distwin" --workpath "../distwin/temp" --specpath "../distwin/temp/" --hidden-import queue -n "Scanner3" -i "Resources/Icon.ico" -w -y -F
 rmdir "../distwin/temp" /s /q
+cd ..
 pause
