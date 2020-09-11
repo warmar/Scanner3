0ca0f7ea025e355f783998af6331b97364f1a66e
Warwick Marangos <warwick.marangos@gmail.com>
2020-09-10 21:09:41 -0400

    Update windows build script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd src
-pyinstaller __main__.py --windowed --noconfirm --onefile --name="Scanner3" --icon="Resources/Icon.ico" --distpath="../distwin" --workpath="../distwin/temp" --specpath="../distwin/temp/" --hidden-import=queue
+pyinstaller __main__.py --windowed --noconfirm --onefile --name="Scanner3" --icon="%cd%/Resources/Icon.ico" --distpath="../distwin" --workpath="../distwin/temp" --specpath="../distwin/temp/" --hidden-import=queue
 rmdir "../distwin/temp" /s /q
 cd ..
 pause
